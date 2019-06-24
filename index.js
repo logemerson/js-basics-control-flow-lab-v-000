1a47d9ec1c0d754a36246980b39bd706d2db65f4
Logan Emerson <logemerson@gmail.com>
2019-06-24 19:16:17 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 // Write your code in this file!
-let fare = 400;
+let fare = '';
 let distance = 0;
 
-function scuberGreetingForFeet(fare, distance) {
-  if (fare <= 400) {
-    return 'This one is on me!'
+function scuberGreetingForFeet(distance) {
+  if (distance <= 400) {
+    return 'This one is on me!';
   } else if (distance > 2000) {
-    return 'I will gladly take your thirty bucks.'
+    return 'I will gladly take your thirty bucks.';
   }
   
   
