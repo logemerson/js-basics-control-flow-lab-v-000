0bcbcd87750415966af738db456c84757e623f24
Logan Emerson <logemerson@gmail.com>
2019-06-24 19:16:55 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -7,6 +7,8 @@ function scuberGreetingForFeet(distance) {
     return 'This one is on me!';
   } else if (distance > 2000) {
     return 'I will gladly take your thirty bucks.';
+  } else if (distance > 2500) {
+    return 'No can do.';
   }
   
   
